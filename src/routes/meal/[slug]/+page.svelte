e0365913fd2c3b57698487278f0c14c1e7a53b58
sabrina <sabrinaabrodi20@gmail.com>
2023-05-08 22:06:45 +0200

    feat: layout single meal

--- src/routes/meal/[slug]/+page.svelte
+++ src/routes/meal/[slug]/+page.svelte
@@ -3,6 +3,35 @@
   export let data;
 </script>
 
-<h3 class="text-lg font-medium text-gray-900 truncate">
-  {data.meal.strMeal}
-</h3>
+<div
+  class="max-w-6xl mx-auto py-12 px-4 sm:px-6 lg:px-8 flex justify-between flex-row max-w-4xl"
+>
+  <div class="bg-white rounded-lg shadow overflow-hidden flex flex-col">
+    <img
+      class="h-64 w-full object-cover"
+      src={data.meal.strMealThumb}
+      alt={data.meal.strMeal}
+    />
+
+    <div class="p-2">
+      <h2 class="text-xl font-medium text-gray-900 mb-3">
+        {data.meal.strMeal}
+      </h2>
+      <div class="flex flex-row">
+        <p class=" mb-2 text-white rounded bg-gray-800 p-2 mr-3">
+          {data.meal.strArea}
+        </p>
+        <p class=" mb-2 text-white rounded bg-gray-800 p-2">
+          {data.meal.strCategory}
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div
+    class="bg-white rounded-lg shadow overflow-hidden flex flex-col w-1/2 p-5"
+  >
+    <h3 class="text-lg font-medium text-gray-900 mb-2">Instructions</h3>
+    <p class="text-gray-700">{data.meal.strInstructions}</p>
+  </div>
+</div>
