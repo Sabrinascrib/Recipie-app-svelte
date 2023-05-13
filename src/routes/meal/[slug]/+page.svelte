0c4291fa4634988882f534073cbb4ff92cc46337
sabrina <sabrinaabrodi20@gmail.com>
2023-05-13 08:25:28 +0200

    fix: styling

--- src/routes/meal/[slug]/+page.svelte
+++ src/routes/meal/[slug]/+page.svelte
@@ -3,35 +3,56 @@
   export let data;
 </script>
 
+<a href="/"> <h1 class="uppercase text-center text-[60px]">Cookspire</h1></a>
+
+<p class="text-center text-[17.5px] mb-16">Cook with whatever you got home!</p>
+
 <div
-  class="max-w-6xl mx-auto py-12 px-4 sm:px-6 lg:px-8 flex justify-between flex-row max-w-4xl"
+  class="max-w-5xl mx-auto py-12 px-4 sm:px-6 lg:px-8 flex justify-between flex-col"
 >
-  <div class="bg-white rounded-lg shadow overflow-hidden flex flex-col">
+  <div class="bg-white rounded-sm shadow overflow-hidden flex flex-col">
     <img
       class="h-64 w-full object-cover"
       src={data.meal.strMealThumb}
       alt={data.meal.strMeal}
     />
 
-    <div class="p-2">
-      <h3 class="text-xl font-medium text-gray-900 mb-3">
+    <div class="px-7 pb-7 pt-5">
+      <h3 class="text-4xl font-medium text-gray-900 uppercase mb-3">
         {data.meal.strMeal}
       </h3>
       <div class="flex flex-row">
-        <p class=" mb-2 text-white rounded bg-gray-800 p-2 mr-3">
+        <p
+          class="bg-green-400 text-green-950 text-center place-items-center flex font-bold px-2 h-7 rounded-sm mr-3"
+        >
           {data.meal.strArea}
         </p>
-        <p class=" mb-2 text-white rounded bg-gray-800 p-2">
+        <p
+          class="bg-green-400 text-green-950 text-center place-items-center flex font-bold px-2 h-7 rounded-sm"
+        >
           {data.meal.strCategory}
         </p>
       </div>
     </div>
   </div>
 
-  <div
-    class="bg-white rounded-lg shadow overflow-hidden flex flex-col w-1/2 p-5"
-  >
-    <h3 class="text-lg font-medium text-gray-900 mb-2">Instructions</h3>
+  <div class="max-w-5xl bg-white rounded-sm shadow flex flex-col mt-5 p-7">
+    <h3 class="text-4xl font-medium uppercase text-gray-900 mb-5">
+      Instructions
+    </h3>
     <p class="text-gray-700">{data.meal.strInstructions}</p>
   </div>
 </div>
+
+<style>
+  a,
+  p {
+    font-family: "Poppins", sans-serif;
+    font-weight: 500;
+  }
+
+  h1,
+  h3 {
+    font-family: "Darumadrop One", cursive;
+  }
+</style>
